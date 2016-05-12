3f2ae18daba9f85e80c8fb58373708694af25420
Rafael <Rafael@Arnhold>
2016-05-11 21:04:17 -0300

    Trabalho Dois ou Um

---
+++ JavaBasicoDoisOuUm/src/br/arnhold/Player.java
@@ -0,0 +1,18 @@
+package br.arnhold;
+
+public class Player {
+	
+	private String nome;
+
+	public Player(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	
+	
+
+}
---
+++ JavaBasicoDoisOuUm/src/br/arnhold/Principal.java
@@ -0,0 +1,14 @@
+package br.arnhold;
+
+import javax.swing.JOptionPane;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		int qtdeJogadores = Integer.parseInt(JOptionPane.showInputDialog("Quantos Jogadores iram participar?"));
+		
+		
+		
+	}
+
+}
