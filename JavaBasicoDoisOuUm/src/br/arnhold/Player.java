e7ee7101752abb9ca2573b77de204e7c974234ef
Rafael <Rafael@Arnhold>
2016-05-14 16:37:32 -0300

    start da partida

--- JavaBasicoDoisOuUm/src/br/arnhold/Player.java
+++ JavaBasicoDoisOuUm/src/br/arnhold/Player.java
@@ -3,6 +3,15 @@ package br.arnhold;
 public class Player {
 	
 	private String nome;
+	private int valorjogado;
+
+	public int getValorjogado() {
+		return valorjogado;
+	}
+
+	public void setValorjogado(int valorjogado) {
+		this.valorjogado = valorjogado;
+	}
 
 	public Player(String nome) {
 		super();
--- JavaBasicoDoisOuUm/src/br/arnhold/Principal.java
+++ JavaBasicoDoisOuUm/src/br/arnhold/Principal.java
@@ -2,11 +2,14 @@ package br.arnhold;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.concurrent.SynchronousQueue;
+
+import javax.swing.JOptionPane;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		int qtdeJogadores = 3;// Integer.parseInt(JOptionPane.showInputDialog("Quantos
+		int qtdeJogadores = 3; // Integer.parseInt(JOptionPane.showInputDialog("Quantos
 								// Jogadores iram participar?"));
 
 		ArrayList<Player> jogadores = new ArrayList<>();
@@ -16,38 +19,44 @@ public class Principal {
 		} else {
 			for (int i = 0; i < qtdeJogadores; i++) {
 				Player pi = new Player("Jogador" + i);
-				//Adiciona os jogadores ao ArrayList
+				// Adiciona os jogadores ao ArrayList
 				jogadores.add(pi);
-/*
-				System.out.print(jogadores.get(i).getNome()+" Escolheu: " );
-				System.out.println(escolheNumero(jogadores));*/
+				System.out.println(jogadores.get(i).getNome());
 			}
-			
-			doisOuUm(jogadores);
-			
-			
-			parOuImpar(jogadores);
 
+			startPartida(jogadores);
 		}
+
+	}
+
+	private static void startPartida(ArrayList<Player> jogadores) {
+		int tirouUm = 0;
+		int tirouDois = 0;
+		
+	while (jogadores.size() != 2) {
+			//doisOuUm(jogadores);
+		
+	}
+		
+	
+			
+	
+		
+
 	}
 
 	private static void doisOuUm(ArrayList<Player> jogadores) {
-		while (jogadores.size() != 2) {
-			doisOuUm(jogadores);	
-			
+		for (int i = 0; i < jogadores.size(); i++) {
+			escolheNumero(jogadores.get(i));
+			System.out.println(jogadores.get(i).getNome() + " tirou " + jogadores.get(i).getValorjogado());
 		}
 		
 	}
 
-	private static void parOuImpar(ArrayList<Player> jogadores) {
-		// TODO Auto-generated method stub
-
-	}
-
-	private static int escolheNumero(ArrayList<Player> jogadores) {
+	private static void escolheNumero(Player player) {
 		Random ran = new Random();
 		// é incluido o + 1 Para sempre gerar 1 ou 2
-		return (ran.nextInt(2) + 1);
+		player.setValorjogado(ran.nextInt(2) + 1);
 
 	}
 
